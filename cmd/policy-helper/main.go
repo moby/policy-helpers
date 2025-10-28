834c3f82810eddfa40219d3b1e76118ea86f521c
Tonis Tiigi <tonistiigi@gmail.com>
2025-10-28 15:05:03 -0700

    artifact verifier base
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- cmd/policy-helper/main.go
+++ cmd/policy-helper/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"os"
 
 	policy "github.com/moby/policy-helpers"
+	"github.com/moby/policy-helpers/githubapi"
+	"github.com/opencontainers/go-digest"
+	"github.com/pkg/errors"
 	"github.com/theupdateframework/go-tuf/v2/metadata"
 )
 
@@ -19,30 +24,97 @@ func main() {
 }
 
 func run() error {
-	var stateDir string
-	var requireOnline bool
-	var debug bool
-	flag.StringVar(&stateDir, "state-dir", "", "Path to state directory")
-	flag.BoolVar(&requireOnline, "require-online", false, "Require online TUF roots update")
-	flag.BoolVar(&debug, "debug", false, "Enable debug logging")
+	var opts struct {
+		stateDir      string
+		requireOnline bool
+		debug         bool
+		bundle        string
+		repo          string
+	}
+	flag.StringVar(&opts.stateDir, "state-dir", "", "Path to state directory")
+	flag.BoolVar(&opts.requireOnline, "require-online", false, "Require online TUF roots update")
+	flag.BoolVar(&opts.debug, "debug", false, "Enable debug logging")
+	flag.StringVar(&opts.bundle, "bundle", "", "Path to attestation bundle file (if empty, will pull from GitHub)")
+	flag.StringVar(&opts.repo, "repo", "", "GitHub repository to pull attestation from (owner/repo)")
 
 	flag.Parse()
 
-	if debug {
+	args := flag.Args()
+	if len(args) == 0 {
+		return errors.Errorf("no command specified")
+	}
+
+	if opts.debug {
 		metadata.SetLogger(&tufLogger{l: slog.Default()})
 	}
 
 	cfg := policy.Config{
-		StateDir:     stateDir,
-		RquireOnline: requireOnline,
+		StateDir:      opts.stateDir,
+		RequireOnline: opts.requireOnline,
 	}
 	v, err := policy.NewVerifier(cfg)
 	if err != nil {
 		return err
 	}
 
-	_ = v // use the verifier as needed
+	ctx := context.TODO()
+
+	switch args[0] {
+	case "artifact":
+		args := args[1:]
+		if len(args) == 0 {
+			return errors.Errorf("no artifact path specified")
+		}
+		return runArtifactCmd(ctx, v, args[0], opts.bundle, opts.repo)
+	default:
+		return errors.Errorf("unknown command: %s", args[0])
+	}
+}
+
+func runArtifactCmd(ctx context.Context, v *policy.Verifier, artifactPath string, bundlePath, repo string) error {
+	var rc io.ReadCloser
+	if artifactPath == "-" {
+		rc = io.NopCloser(os.Stdin)
+	} else {
+		f, err := os.Open(artifactPath)
+		if err != nil {
+			return errors.Wrapf(err, "opening artifact file %q", artifactPath)
+		}
+		rc = f
+	}
+
+	dgst, err := digest.FromReader(rc)
+	if err != nil {
+		rc.Close()
+		return errors.Wrapf(err, "computing digest for artifact %q", artifactPath)
+	}
+
+	rc.Close()
+
+	var bundleBytes []byte
+	if bundlePath != "" {
+		b, err := os.ReadFile(bundlePath)
+		if err != nil {
+			return errors.Wrapf(err, "reading bundle file %q", bundlePath)
+		}
+		bundleBytes = b
+	} else if repo != "" {
+		bundleBytes, err = githubapi.PullAttestation(ctx, nil, dgst, repo)
+		if err != nil {
+			return errors.Wrapf(err, "pulling attestation from repo %q", repo)
+		}
+	} else {
+		return errors.Errorf("either bundle path or repo must be specified")
+	}
+
+	verified, err := v.VerifyArtifact(ctx, dgst, bundleBytes)
+	if err != nil {
+		return errors.Wrapf(err, "verifying artifact %q", artifactPath)
+	}
+
+	log.Printf("artifact %q verified: %+v", artifactPath, verified)
 	return nil
+
 }
 
 type tufLogger struct {
--- verifier.go
+++ verifier.go
@@ -2,18 +2,23 @@ package verifier
 
 import (
 	"context"
-	"log"
+	"encoding/hex"
 	"path/filepath"
 	"time"
 
 	"github.com/moby/policy-helpers/roots"
+	"github.com/opencontainers/go-digest"
 	"github.com/pkg/errors"
+	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	"github.com/sigstore/sigstore-go/pkg/bundle"
+	"github.com/sigstore/sigstore-go/pkg/fulcio/certificate"
+	"github.com/sigstore/sigstore-go/pkg/verify"
 	"golang.org/x/sync/singleflight"
 )
 
 type Config struct {
 	UpdateInterval time.Duration
-	RquireOnline   bool
+	RequireOnline  bool
 	StateDir       string
 }
 
@@ -23,25 +28,67 @@ type Verifier struct {
 	tp  *roots.TrustProvider // tp may be nil if initialization failed
 }
 
+type SignatureInfo struct {
+	Signer     certificate.Summary                  `json:"signature"`
+	Timestamps []verify.TimestampVerificationResult `json:"timestamps"`
+}
+
 func NewVerifier(cfg Config) (*Verifier, error) {
 	if cfg.StateDir == "" {
 		return nil, errors.Errorf("state directory must be provided")
 	}
 	v := &Verifier{cfg: cfg}
 
-	tp, err := v.loadTrustProvider() // initialization fails on expired root/timestamp
+	v.loadTrustProvider() // initialization fails on expired root/timestamp
 
-	if err == nil {
-		tm, st, err := tp.TrustedRoot(context.TODO())
-		if err != nil {
-			return nil, err
-		}
-		if st.Error != nil {
-			log.Printf("trusted root warning: %+v", st.Error)
-		}
-		log.Printf("tm: %+v", tm)
-	}
 	return v, nil
+}
+
+func (v *Verifier) VerifyArtifact(ctx context.Context, dgst digest.Digest, bundleBytes []byte) (*SignatureInfo, error) {
+	anyCert, err := anyCerificateIdentity()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	alg, rawDgst, err := rawDigest(dgst)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	policy := verify.NewPolicy(verify.WithArtifactDigest(alg, rawDgst), anyCert)
+
+	b, err := loadBundle(bundleBytes)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	tp, err := v.loadTrustProvider()
+	if err != nil {
+		return nil, errors.Wrap(err, "loading trust provider")
+	}
+
+	trustedRoot, _, err := tp.TrustedRoot(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting trusted root")
+	}
+
+	gv, err := verify.NewVerifier(trustedRoot, verify.WithSignedCertificateTimestamps(1), verify.WithTransparencyLog(1), verify.WithObserverTimestamps(1))
+	if err != nil {
+		return nil, errors.Wrap(err, "creating verifier")
+	}
+
+	result, err := gv.Verify(b, policy)
+	if err != nil {
+		return nil, errors.Wrap(err, "verifying bundle")
+	}
+
+	if result.Signature == nil || result.Signature.Certificate == nil {
+		return nil, errors.Errorf("no valid signatures found")
+	}
+
+	si := &SignatureInfo{}
+	si.Signer = *result.Signature.Certificate
+	si.Timestamps = result.VerifiedTimestamps
+
+	return si, nil
 }
 
 func (v *Verifier) loadTrustProvider() (*roots.TrustProvider, error) {
@@ -51,19 +98,65 @@ func (v *Verifier) loadTrustProvider() (*roots.TrustProvider, error) {
 			tpCache = v.tp
 			return nil, nil
 		}
-		tpCache, err := roots.NewTrustProvider(roots.SigstoreRootsConfig{
+		tp, err := roots.NewTrustProvider(roots.SigstoreRootsConfig{
 			CachePath:      filepath.Join(v.cfg.StateDir, "tuf"),
 			UpdateInterval: v.cfg.UpdateInterval,
-			RequireOnline:  v.cfg.RquireOnline,
+			RequireOnline:  v.cfg.RequireOnline,
 		})
 		if err != nil {
 			return nil, err
 		}
-		v.tp = tpCache
+		v.tp = tp
+		tpCache = tp
 		return nil, nil
 	})
 	if err != nil {
 		return nil, err
 	}
 	return tpCache, nil
+}
+
+func anyCerificateIdentity() (verify.PolicyOption, error) {
+	sanMatcher, err := verify.NewSANMatcher("", ".*")
+	if err != nil {
+
+		return nil, err
+	}
+
+	issuerMatcher, err := verify.NewIssuerMatcher("", ".*")
+	if err != nil {
+		return nil, err
+	}
+
+	extensions := certificate.Extensions{
+		RunnerEnvironment: "github-hosted",
+	}
+
+	certId, err := verify.NewCertificateIdentity(sanMatcher, issuerMatcher, extensions)
+	if err != nil {
+		return nil, err
+	}
+
+	return verify.WithCertificateIdentity(certId), nil
+}
+
+func loadBundle(dt []byte) (*bundle.Bundle, error) {
+	var bundle bundle.Bundle
+	bundle.Bundle = new(protobundle.Bundle)
+
+	err := bundle.UnmarshalJSON(dt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bundle, nil
+}
+
+func rawDigest(d digest.Digest) (string, []byte, error) {
+	alg := d.Algorithm().String()
+	b, err := hex.DecodeString(d.Encoded())
+	if err != nil {
+		return "", nil, errors.Wrapf(err, "decoding digest %s", d)
+	}
+	return alg, b, nil
 }
